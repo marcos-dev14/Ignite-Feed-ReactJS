38f4526d7b49d5c75e89e103a8df78351698a404
marcos-dev14 <mprogramador.10@gmail.com>
2022-09-05 12:03:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from './components/Header';
+
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <h1>Marcos</h1>
+    </>
+  )
+}
